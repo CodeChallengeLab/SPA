9cfb7a27dec13afac24b1b914539a019db237fb0
MariaGins <m.gins@mail.ru>
2025-06-11 10:44:29 +0300

    Posts and Users on the page with pagination

--- src/components/features/albums/AlbumsGrid.tsx
+++ src/components/features/albums/AlbumsGrid.tsx
@@ -6,14 +6,14 @@ import LoadingSpinner from "../../LoadingSpinner";
 import { AlbumCard } from "./AlbumCard";
 import { SimplePagination } from "../../layout/SimplePagination";
 import { useState, type FC } from "react";
+import { AlbumsStore } from "../../../state-management/AlbumStore";
 interface Props {
   itemsPerPageParam: number;
 }
 const AlbumsGrid: FC<Props> = observer(({itemsPerPageParam}) => {  
   const { albumsStore } = rootStore;
-  const [startIndex, setStartIndex] = useState(0);
-  const [itemsPerPage, setItemsPerPage] = useState(itemsPerPageParam); 
-  console.log(startIndex);
+  const [page, setPage] = useState(albumsStore.currentPage);
+  const [itemsPerPage, setItemsPerPage] = useState(itemsPerPageParam);   
   if (albumsStore.isLoading) {
 
     return <LoadingSpinner message="Loading albums..." />;
@@ -30,7 +30,7 @@ const AlbumsGrid: FC<Props> = observer(({itemsPerPageParam}) => {
       {albumsStore.albums.length > 0 && (
         <>
           <Grid container spacing={3}>
-            {albumsStore.albums.slice(startIndex, startIndex + itemsPerPage).map((album) => (
+            {albumsStore.albums.slice(page*itemsPerPage - itemsPerPage, page*itemsPerPage).map((album) => (
               <Grid key={album.id} size={{ xs: 12, sm: 6, md: 4, lg: 3 }}>
                 <AlbumCard album={album} />
               </Grid>
@@ -38,10 +38,11 @@ const AlbumsGrid: FC<Props> = observer(({itemsPerPageParam}) => {
           </Grid>
           <SimplePagination                    
             totalItems={albumsStore.albums.length}           
-            onPageChange={(index) => {setStartIndex(index)}}     
+            onPageChange={(page) => {setPage(page); albumsStore.currentPage = page;}}     
             onItemsPerPageChange={(count) => {setItemsPerPage(count)}}     
             gridName="albums"
             itemsPerPageParam={itemsPerPage}
+            store={albumsStore as AlbumsStore}
           />
         </>
       )}
